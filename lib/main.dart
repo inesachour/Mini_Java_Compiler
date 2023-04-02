156b399fa53c9be5d039aba4bf347b2c778953f6
Ines Achour <inesachour@insat.u-carthage.tn>
2023-04-02 14:19:14 +0100

    screen structure

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
   }
@@ -26,7 +27,104 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    double deviceHeight = MediaQuery.of(context).size.height;
+    double deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.blue,
+              //height: deviceHeight*0.1,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: deviceWidth *0.03),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      child: Text("Importer fichier", style: TextStyle(color: Colors.blue),),
+                      onPressed: (){
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.white)
+                      ),
+                    ),
+                    ElevatedButton(
+                      child: Text("Compiler", style: TextStyle(color: Colors.blue)),
+                      onPressed: (){
+                      },
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.white)
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 8,
+            child: Padding(
+              padding: EdgeInsets.only(left: deviceWidth*0.1,),
+              child: Container(
+                decoration: BoxDecoration(
+                  //border: Border.all(color: Colors.black,),
+                  color: Colors.white
+                ),
+                child: TextField(
+                  keyboardType: TextInputType.multiline,
+                  cursorColor: Colors.grey,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    color: Colors.grey.shade300,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+                      child: Text("Console", style: TextStyle(fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          controller: TextEditingController(text: "OK"),
+                          enabled: false,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
